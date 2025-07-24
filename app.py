3b2086d775a4acd8780ed7bcec5b2155e7a7a7d2
devops <devops@Host.lan>
2025-07-24 18:36:21 +0200

    test ci

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"Hello from Flask in Docker! DB_HOST={os.environ.get('DB_HOST')}"
+    return f"Testing GitHub Actions pipelines"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
