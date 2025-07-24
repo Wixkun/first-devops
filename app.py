31ad5e807be95e6c562db11f057ac79a41e03999
devops <devops@Host.lan>
2025-07-24 19:39:43 +0200

    cleaningPipes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"More complete pipeline"
+    return f"Cleaning pipelines"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
